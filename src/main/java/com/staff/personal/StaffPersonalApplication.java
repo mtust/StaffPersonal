f4f5b17f51b850d72c27c5311eddf25392ae9d03
mtust <mtustanovskyy@gmail.com>
2016-10-13 21:19:58 +0300

    first commit

---
+++ src/main/java/com/staff/personal/StaffPersonalApplication.java
@@ -0,0 +1,12 @@
+package com.staff.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffPersonalApplication.class, args);
+	}
+}
